210a3e8710c92c1c716a2e0a8bcf42a9831a9d70
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-05-17 19:32:13 +0600

    Middleware In Express.Js

--- dist/app/app.js
+++ dist/app/app.js
@@ -5,7 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
+const port = process.env.PORT || 3000;
+//parser
+app.use(express_1.default.json());
+app.use(express_1.default.text());
+const logger = (req, res, next) => {
+    console.log(req.method, req.url, req.hostname);
+    next();
+};
+app.get("", logger, (req, res) => {
     res.send("Hello World!");
+});
+app.post("/", logger, (req, res) => {
+    console.log(req.body);
+    // res.send("Hello World!");
+    res.json({ "success": "success" });
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,26 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, {  NextFunction, Request, Response } from "express";
+
 const app = express();
-app.get("/", (req, res) => {
+
+const port = process.env.PORT || 3000;
+
+//parser
+ app.use(express.json());
+ app.use(express.text());
+
+ const logger = (req:Request, res:Response, next:NextFunction) => {
+    console.log(req.method, req.url, req.hostname, );
+    next();
+ }
+
+app.get("", logger, (req:Request, res:Response, ) => {
   res.send("Hello World!");
 });
+
+app.post("/", logger, (req:Request, res:Response,) => {
+    console.log(req.body);
+    // res.send("Hello World!");
+    res.json({"success": "success"})
+}); 
 
 export default app;
